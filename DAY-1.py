94acf37c21a90825e60b1fd77a964cf77b6b6d5f
ayush41 <bhattayushraj@gmail.com>
2023-12-12 11:49:10 +0530

    intialised

---
+++ DAY-1.py
@@ -0,0 +1,13 @@
+import numpy as np;
+import pandas as pd;
+import matplotlib.pyplot as plt;
+import warnings;
+import seaborn as sb;
+warnings.filterwarnings("ignore")
+# Load the dataset
+
+print("program initialized")
+dataset = pd.read_csv('bank.csv')
+
+# Display the first few rows of the dataset
+print(dataset.head())
