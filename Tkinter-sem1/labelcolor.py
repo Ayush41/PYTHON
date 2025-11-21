2463c807fffb21ae95a7761c32230e722cb33f2a
ayush41 <anonymousking41681@gmail.com>
2025-11-21 10:45:47 +0530

    bg-color

--- Tkinter-sem1/labelcolor.py
+++ Tkinter-sem1/labelcolor.py
@@ -5,6 +5,6 @@ root.title("Colored Label")
 
 label = tk.Label(root,text="This is colored label",fg="red",bg="yellow",font=("Arial",20))
 label.pack(padx=50,pady=40)
-
+root.configure(bg="green")
 
 root.mainloop()
